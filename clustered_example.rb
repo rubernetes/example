d84a8cc010c777b7f2d5eeb728d7c0b2bc734aa1
Alaa <soupermado@gmail.com>
2023-03-09 14:52:52 +0200

    [Update] readme and docs

--- clustered_example.rb
+++ clustered_example.rb
@@ -3,11 +3,17 @@
 require 'rubernetes'
 
 # Create a new operator that handlle the events callbacks
+# @param crd_group [string] ApiGroup from crd
+# @param crd_version [string] ApiVersion from crd
+# @param crd_plural [string] Name (plural) from crd
+# @param options [Hash] Additional options
 class CustomOperatorController < Rubernetes::Operator
   def initialize(crd_group, crd_version, crd_plural, options = {})
     super(crd_group, crd_version, crd_plural, options)
   end
 
+  # Event callback when a custom resource is added, you can add your cutom logic here that will be triggered when a custom resource is added
+  # @param event [Hash] Event from the watcher that contains all information about the custom resource that you will need
   def added(event)
     @logger.info 'new custom resource has been added on the cluster'
     # custom logic to handle the creation of a custom resource
@@ -17,6 +23,8 @@ class CustomOperatorController < Rubernetes::Operator
     # @status = get_status(event)
   end
 
+  # Event callback when a custom resource is modified, you can add your cutom logic here that will be triggered when a custom resource is modified
+  # @param event [Hash] Event from the watcher that contains all information about the custom resource that you will need
   def modified(event)
     @logger.info 'a custom resource has been modified on the cluster'
     # custom logic to handle the modification of a custom resource
@@ -24,6 +32,8 @@ class CustomOperatorController < Rubernetes::Operator
     @logger.info event
   end
 
+  # Event callback when a custom resource is deleted, you can add your cutom logic here that will be triggered when a custom resource is deleted
+  # @param event [Hash] Event from the watcher that contains all information about the custom resource that you will need
   def deleted(_event)
     @logger.info 'a custom resource has been deleted from the cluster'
     # custom logic to handle the deletion of a custom resource
